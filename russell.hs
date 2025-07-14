88c1a2009cd062b9902036ad5a7d38ef52f78fb6
Alex <alex.podikoglou@gmail.com>
2025-07-14 20:33:52 +0300

    format

--- russell.hs
+++ russell.hs
@@ -6,21 +6,20 @@ data ASTNode
   | Negation ASTNode
   | Conjunction ASTNode ASTNode
   | Disjunction ASTNode ASTNode
-  -- | XOR ASTNode ASTNode
-  -- | Implication ASTNode ASTNode
-  | Equality ASTNode ASTNode
+  | -- | XOR ASTNode ASTNode
+    -- | Implication ASTNode ASTNode
+    Equality ASTNode ASTNode
 
 type Assignments = Data.Map.Map Char Bool
 
 eval :: ASTNode -> Assignments -> Maybe Bool
 eval node assignments = case node of
-  Literal val         -> Just val
-  Variable name       -> Data.Map.lookup name assignments
-
-  Negation node -> fmap not (eval node assignments) 
+  Literal val -> Just val
+  Variable name -> Data.Map.lookup name assignments
+  Negation node -> fmap not (eval node assignments)
   Conjunction lhs rhs -> liftA2 (&&) (eval lhs assignments) (eval rhs assignments)
   Disjunction lhs rhs -> liftA2 (||) (eval lhs assignments) (eval rhs assignments)
-  Equality    lhs rhs -> liftA2 (==) (eval lhs assignments) (eval rhs assignments)
+  Equality lhs rhs -> liftA2 (==) (eval lhs assignments) (eval rhs assignments)
 
 computeTruthTable :: ASTNode -> [Assignments]
 computeTruthTable node = []
@@ -37,12 +36,15 @@ isContingency node =
     (\l -> eval node l == Just False)
     (computeTruthTable node)
 
-ast = Equality
-  (Disjunction 
-    (Variable 'p') 
-    (Conjunction
-      (Variable 'p')
-      (Variable 'q')))
-  (Variable 'p')
+ast =
+  Equality
+    ( Disjunction
+        (Variable 'p')
+        ( Conjunction
+            (Variable 'p')
+            (Variable 'q')
+        )
+    )
+    (Variable 'p')
 
-main = print (eval ast (Data.Map.fromList [ ('p', True), ('q', True) ]))
+main = print (eval ast (Data.Map.fromList [('p', True), ('q', True)]))
